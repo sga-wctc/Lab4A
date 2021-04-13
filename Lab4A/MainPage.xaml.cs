109129ad7b3d773947999d552c47b60490bde0b5
sga-wctc <sakins@my.wctc.edu>
2021-04-12 20:02:51 -0500

    Add project files.

---
+++ Lab4A/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab4A
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void DotEntered(object sender, System.EventArgs e)
+        {
+            var str = entryLabel.Text;
+            entryLabel.Text = str + '.';
+        }
+        void DashEntered(object sender, System.EventArgs e)
+        {
+            var str = entryLabel.Text;
+            entryLabel.Text = str + '-';
+        }
+        void SpaceEntered(object sender, System.EventArgs e)
+        {
+            var str = entryLabel.Text;
+            char newChar = Morse.MorseCoder(str);
+            str = messageLabel.Text;
+            messageLabel.Text = str + newChar + " ";
+            entryLabel.Text = "";
+
+        }
+
+    }
+
+}
+
